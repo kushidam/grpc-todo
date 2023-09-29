156295ec475cb0493b7e7705d3958fdb80b5a0ac
kushidam <131370475+kushidam@users.noreply.github.com>
2023-09-29 21:53:41 +0900

    [Add] テーブル駆動テスト

--- cmd/server/createTodo_test.go
+++ cmd/server/createTodo_test.go
@@ -9,23 +9,45 @@ import (
 )
 
 func TestCreateTodo(t *testing.T) {
-	Todoer := &TodoServer{}
-	// testUuid := uuid.New().String()
 	
-	reqItem := &todov1.CreateTodoRequest{
-		Title:   "test1 title",
-	}
-	reqTodo := &connect.Request[todov1.CreateTodoRequest]{
-		Msg: reqItem,
+	tests := []struct {
+		name           string
+		request        *connect.Request[todov1.CreateTodoRequest]
+		expectedTitle  string
+	}{
+		{
+			name: "Create Todo with valid input",
+			request: &connect.Request[todov1.CreateTodoRequest]{
+				Msg: &todov1.CreateTodoRequest{
+					Title: "test1 title",
+				},
+			},
+			expectedTitle: "test1 title",
+		},
+		{
+			name: "Create Todo with empty string",
+			request: &connect.Request[todov1.CreateTodoRequest]{
+				Msg: &todov1.CreateTodoRequest{
+					Title: "",
+				},
+			},
+			expectedTitle: "",
+		},
+		// シナリオのテストケースをここに追加
 	}
 
-	expected := "test1 title"
-	actualResult, err := Todoer.CreateTodo(context.TODO(), reqTodo)
+	Todoer := &TodoServer{}
 
-	if err != nil {
-		t.Fatalf("CreateTodo failed with error: %v", err)
-	}
-	if actualResult.Msg.Item.Title != expected {
-		t.Errorf("actualResult [%v] is not equal to expected [%v]", actualResult.Msg.Item.Title, expected)
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			actualResult, err := Todoer.CreateTodo(context.TODO(), test.request)
+
+			if err != nil {
+				t.Fatalf("CreateTodo failed with error: %v", err)
+			}
+			if actualResult.Msg.Item.Title != test.expectedTitle {
+				t.Errorf("actualResult [%v] is not equal to expected [%v]", actualResult.Msg.Item.Title, test.expectedTitle)
+			}
+		})
 	}
 }
