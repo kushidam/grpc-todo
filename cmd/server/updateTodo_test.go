edb8aaff7f006ce8fa69c5a037dc13c0ee1795c1
kushidam <131370475+kushidam@users.noreply.github.com>
2023-10-02 23:25:24 +0900

    [Add]  UpdateTodo TDT移行

--- cmd/server/updateTodo_test.go
+++ cmd/server/updateTodo_test.go
@@ -20,23 +20,67 @@ func TestUpdateTodo(t *testing.T) {
 	}
 	Todoer.items.Store(testUuid, testItem)
 
-
-	reqItem := &todov1.UpdateTodoRequest{
-		Id:     testUuid,
-		Status: todov1.TodoItem_STATUS_NOSTARTED,
+	tests := []struct {
+		name           string
+		request        *connect.Request[todov1.UpdateTodoRequest]
+		requestStatus  todov1.TodoItem_Status
+		expectedStatus todov1.TodoItem_Status
+		expectError    bool
+	}{
+		{
+			name:           "Update to COMPLETED",
+			request: &connect.Request[todov1.UpdateTodoRequest]{
+				Msg: &todov1.UpdateTodoRequest{
+					Id: testUuid,
+				},
+			},
+			requestStatus:  todov1.TodoItem_STATUS_COMPLETED,
+			expectedStatus: todov1.TodoItem_STATUS_COMPLETED,
+			expectError:    false,
+		},
+		{
+			name:           "Update to NOSTARTED",
+			request: &connect.Request[todov1.UpdateTodoRequest]{
+				Msg: &todov1.UpdateTodoRequest{
+					Id: testUuid,
+				},
+			},
+			requestStatus:  todov1.TodoItem_STATUS_NOSTARTED,
+			expectedStatus: todov1.TodoItem_STATUS_NOSTARTED,
+			expectError:    false,
+		},
+		{
+			name:           "Todo item not found",
+			request: &connect.Request[todov1.UpdateTodoRequest]{
+				Msg: &todov1.UpdateTodoRequest{
+					Id: "1234567",
+				},
+			},
+			expectError:    true,
+		},
+		// シナリオのテストケースをここに追加
 	}
-	
-	reqTodo := &connect.Request[todov1.UpdateTodoRequest]{
-		Msg: reqItem,
-	}
 
-	expected := todov1.TodoItem_STATUS_COMPLETED
-	actualResult, err := Todoer.UpdateTodo(context.TODO(), reqTodo)
+	// テストケースをループで実行
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
 
-	if err != nil {
-		t.Fatalf("UpdateTodo failed with error: %v", err)
-	}
-	if actualResult.Msg.Item.Status != expected {
-		t.Errorf("actualResult [%v] is not equal to expected [%v]", actualResult.Msg.Item.Title, expected)
+			actualResult, err := Todoer.UpdateTodo(context.TODO(), test.request)
+
+			// エラーが期待通りの場合
+			if test.expectError && err == nil {
+				t.Errorf("Expected an error, but got nil")
+			}
+
+			// エラーが期待通りでない場合
+			if !test.expectError && err != nil {
+				t.Errorf("Expected no error, UpdateTodo failed with error: %v", err)
+			}
+
+			// ステータスが期待通りでない場合
+			if actualResult != nil && actualResult.Msg.Item.Status != test.expectedStatus {
+				t.Errorf("actualResult [%v] is not equal to expected [%v]", actualResult.Msg.Item.Status, test.expectedStatus)
+			}
+		})
 	}
 }
